b0548c04ff78fb2d87ff5cf10ff890e05ea70dfb
Putranugroho14 <putranugroho61@gmail.com>
2024-06-13 11:50:28 +0700

    membuat while

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -60,7 +60,14 @@ public:
 		//This function searches the currentnode of the specified Node as well as the current Node of its parent
 		currentNode = ROOT;
 		parent = nullptr;
-		
+		while ((currentNode != nullptr) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
 	}
 	
 };
